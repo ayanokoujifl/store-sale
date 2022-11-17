204cd07abea8ea44537db914d251585c8b1da13a
Ayanokoujifl <guleite3@gmail.com>
2022-11-17 20:52:24 -0300

    created project

---
+++ CursoMC/src/test/java/com/ayanokoujifl/cursomc/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayanokoujifl.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
