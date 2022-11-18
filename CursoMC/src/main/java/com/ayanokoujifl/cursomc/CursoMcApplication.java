8266e4593392048d3e37938f097ae29684ce92e7
Ayanokoujifl <guleite3@gmail.com>
2022-11-18 10:06:11 -0300

    many-to-many association between products and categories

--- CursoMC/src/main/java/com/ayanokoujifl/cursomc/CursoMcApplication.java
+++ CursoMC/src/main/java/com/ayanokoujifl/cursomc/CursoMcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ayanokoujifl.cursomc.entities.Categoria;
+import com.ayanokoujifl.cursomc.entities.Produto;
 import com.ayanokoujifl.cursomc.repositories.CategoriaRepository;
+import com.ayanokoujifl.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
@@ -19,11 +21,26 @@ public class CursoMcApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-	Categoria cat1=new Categoria(null,"Informática");
-	Categoria cat2=new Categoria(null,"Escritório");
-	categoriaRepository.saveAllAndFlush(Arrays.asList(cat1,cat2));
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAllAndFlush(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAllAndFlush(Arrays.asList(p1, p2, p3));
 	}
 }
--- CursoMC/src/main/java/com/ayanokoujifl/cursomc/entities/Categoria.java
+++ CursoMC/src/main/java/com/ayanokoujifl/cursomc/entities/Categoria.java
@@ -1,12 +1,15 @@
 package com.ayanokoujifl.cursomc.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList();
 
 	public Categoria() {
 		// TODO Auto-generated constructor stub
@@ -63,7 +69,13 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
 }
