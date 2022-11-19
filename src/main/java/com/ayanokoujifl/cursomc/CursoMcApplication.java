921e3e46ffb4c921e6f3522aee6cd29502d2f939
Ayanokoujifl <guleite3@gmail.com>
2022-11-19 00:41:14 -0300

    states and citys

--- src/main/java/com/ayanokoujifl/cursomc/CursoMcApplication.java
+++ src/main/java/com/ayanokoujifl/cursomc/CursoMcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ayanokoujifl.cursomc.entities.Categoria;
+import com.ayanokoujifl.cursomc.entities.Cidade;
+import com.ayanokoujifl.cursomc.entities.Estado;
 import com.ayanokoujifl.cursomc.entities.Produto;
 import com.ayanokoujifl.cursomc.repositories.CategoriaRepository;
+import com.ayanokoujifl.cursomc.repositories.CidadeRepository;
+import com.ayanokoujifl.cursomc.repositories.EstadoRepository;
 import com.ayanokoujifl.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -24,6 +28,12 @@ public class CursoMcApplication implements CommandLineRunner {
 
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -40,6 +50,18 @@ public class CursoMcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+		Estado est1=new Estado(null,"Minas Gerais");
+		Estado est2=new Estado(null,"São Paulo");
+		
+		Cidade c1=new Cidade(null,"Uberlândia",est1);
+		Cidade c2=new Cidade(null,"São Paulo",est2);
+		Cidade c3=new Cidade(null,"Campinas",est2);
+	
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAllAndFlush(Arrays.asList(est1,est2));
+		cidadeRepository.saveAllAndFlush(Arrays.asList(c1,c2,c3));
 		categoriaRepository.saveAllAndFlush(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAllAndFlush(Arrays.asList(p1, p2, p3));
 	}
---
+++ src/main/java/com/ayanokoujifl/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.ayanokoujifl.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ayanokoujifl.cursomc.entities.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
